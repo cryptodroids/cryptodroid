bec9dba45354e24030fca0a9317eabd64b3d862c
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2014-04-11 10:58:03 +0100

    added robolectric test suite

---
+++ secure-preferences/src/test/java/com/cryptodroid/securepreferences/SecureSharedPreferencesTest.java
@@ -0,0 +1,96 @@
+package com.cryptodroid.securepreferences;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by chris on 11/04/2014.
+ * For cryptodroid.
+ */
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class SecureSharedPreferencesTest {
+
+    SecureSharedPreferences secureSharedPreferences;
+
+    @Before
+    public void setUp() throws Exception {
+//        Robolectric.application
+//                secureSharedPreferences = new SecureSharedPreferences()
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDefaultSharedPreference() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSharedPreference() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testEdit() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBoolean() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFloat() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInt() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLong() throws Exception {
+
+    }
+
+    @Test
+    public void testGetString() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStringSet() throws Exception {
+
+    }
+
+    @Test
+    public void testRegisterOnSharedPreferenceChangeListener() throws Exception {
+
+    }
+
+    @Test
+    public void testUnregisterOnSharedPreferenceChangeListener() throws Exception {
+
+    }
+}
